e748a94fc6334abaa2b797aca817e0a54074ec2a
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-09-29 01:07:36 +0530

    Navbar Complete

--- expert-hand-9078/src/Components/AppNavbar.jsx
+++ expert-hand-9078/src/Components/AppNavbar.jsx
@@ -7,130 +7,62 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import styles from "./AppNavbar.module.css";
 import { Link, NavLink } from "react-router-dom";
 function AppNavbar() {
-  // return (
-  //   <div>
-  //     <div className={styles.mainDiv}>
-  //       <ul>
-  //         <NavLink to="/">
-  //           <img
-  //             src="../Images/logo.png"
-  //             style={{ width: "170px", height: "23px" }}
-  //           />
-  //         </NavLink>
-  //         <NavLink to="/">Hours</NavLink>
-  //         <NavLink to="/projects">Projects</NavLink>
-  //         <li className={styles.dropdwon}>
-  //           Reports
-  //           <ul>
-  //             <li>1</li>
-  //             <li>2</li>
-  //             <li>3</li>
-  //             <li>4</li>
-  //             <li>5</li>
-  //           </ul>
-  //         </li>
-  //         <li>Manage</li>
-  //         <li>
-  //           <FiSearch />
-  //         </li>
-  //       </ul>
-  //       <ul>
-  //         <li>
-  //           <AiFillPlayCircle />
-  //         </li>
-  //         <li>
-  //           <AiOutlinePlusCircle />
-  //         </li>
-  //         <li>
-  //           <div style={{ border: "1px solid blue" }}>GET PRO</div>
-  //         </li>
-  //         <li>
-  //           <BiDownload />
-  //         </li>
-  //         <li>
-  //           Delete from Data
-  //           <RiDeleteBin6Line />
-  //         </li>
-  //         <li>Profile</li>
-  //       </ul>
-  //     </div>
-  //   </div>
-  // );
   return (
     <div className={styles.sourceDiv}>
       <div className={styles.mainDiv1}>
-        <ul>
-          <li>
-            <NavLink to="/">
-              <img
-                src="../Images/logo.png"
-                style={{
-                  width: "170px",
-                  height: "25px",
-                }}
-              />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/" style={{ textDecoration: "none" }}>
-              Hours
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/projects" style={{ textDecoration: "none" }}>
-              Projects
-            </NavLink>
-          </li>
-          <li>
-            Reports
-            <ul className={styles.reportDrop}>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
-            </ul>
-          </li>
-          <li>
-            Manage
-            <ul className={styles.reportDrop}>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
-            </ul>
-          </li>
-          <li>
-            <FiSearch style={{ marginTop: "10px" }} />
-          </li>
-        </ul>
-      </div>
-      <div className={styles.mainDiv2}>
-        <h1>
-          <AiFillPlayCircle />
-        </h1>
-        <h1>
-          <AiOutlinePlusCircle />
-        </h1>
-        <div className={styles.getPro}>GET PRO</div>
-        {/* <BiDownload /> */}
-        <div
-          style={{
-            border: "1px solid blue",
-            borderRadius: "5px",
-            padding: "5px",
-            backgroundColor: "#fff7e5",
-            fontWeight: "bold",
-            display: "flex",
-            alignItems: "center",
-            gap: "10px",
-          }}
-        >
-          Delete from Data
-          <RiDeleteBin6Line style={{ color: "brown", fontSize: "20px" }} />
+        <Link to="/">
+          <img src="../Images/logo.png" className={styles.logoImg} />
+        </Link>
+        <Link to="/">Hours</Link>
+        <Link to="/projects">Projects</Link>
+        <div className={styles.divv}>
+          Reports
+          <ul className={styles.reportDrop}>
+            <li>Timesheets</li>
+            <li>Dashboard</li>
+            <li>Project reports</li>
+            <li>User Rrorts</li>
+            <li>Pace</li>
+            <li>Shared reports</li>
+          </ul>
         </div>
-        Profile
+        <div className={styles.divv}>
+          Manage
+          <ul className={styles.reportDrop}>
+            <li>Team</li>
+            <li>Work schedules</li>
+            <li>Clients</li>
+            <li>Custome fields</li>
+            <li>Tracking policies</li>
+            <li>Add-ons</li>
+          </ul>
+        </div>
+        <Link>
+          <FiSearch />
+        </Link>
+      </div>
+
+      <div className={styles.mainDiv2}>
+        <AiFillPlayCircle size={28} />
+        <AiOutlinePlusCircle size={23} />
+        <div className={styles.getPro}>GET PRO</div>
+        <div className={styles.downloadIcon}>
+          <BiDownload />
+          <div className={styles.downloadModal}>
+            <img src="../Images/downloadmodal.png" alt="" />
+          </div>
+        </div>
+        <div className={styles.deleteIcon}>
+          Delete from Data
+          <RiDeleteBin6Line size={15} color="brown" />
+        </div>
+        <div>
+          <img
+            src="http://drive.google.com/uc?export=view&id=12HT3xBQpjAS87a3ThBcbuIVq0S7VK_il"
+            alt="personImg"
+            className={styles.profileImg}
+          />
+        </div>
       </div>
     </div>
   );
