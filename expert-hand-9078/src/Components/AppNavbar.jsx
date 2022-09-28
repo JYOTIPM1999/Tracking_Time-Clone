54be9cd2604277678385df006d12cfaf7b435bd6
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-09-28 15:36:43 +0530

    Navbar

--- expert-hand-9078/src/Components/AppNavbar.jsx
+++ expert-hand-9078/src/Components/AppNavbar.jsx
@@ -7,51 +7,130 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import styles from "./AppNavbar.module.css";
 import { Link, NavLink } from "react-router-dom";
 function AppNavbar() {
+  // return (
+  //   <div>
+  //     <div className={styles.mainDiv}>
+  //       <ul>
+  //         <NavLink to="/">
+  //           <img
+  //             src="../Images/logo.png"
+  //             style={{ width: "170px", height: "23px" }}
+  //           />
+  //         </NavLink>
+  //         <NavLink to="/">Hours</NavLink>
+  //         <NavLink to="/projects">Projects</NavLink>
+  //         <li className={styles.dropdwon}>
+  //           Reports
+  //           <ul>
+  //             <li>1</li>
+  //             <li>2</li>
+  //             <li>3</li>
+  //             <li>4</li>
+  //             <li>5</li>
+  //           </ul>
+  //         </li>
+  //         <li>Manage</li>
+  //         <li>
+  //           <FiSearch />
+  //         </li>
+  //       </ul>
+  //       <ul>
+  //         <li>
+  //           <AiFillPlayCircle />
+  //         </li>
+  //         <li>
+  //           <AiOutlinePlusCircle />
+  //         </li>
+  //         <li>
+  //           <div style={{ border: "1px solid blue" }}>GET PRO</div>
+  //         </li>
+  //         <li>
+  //           <BiDownload />
+  //         </li>
+  //         <li>
+  //           Delete from Data
+  //           <RiDeleteBin6Line />
+  //         </li>
+  //         <li>Profile</li>
+  //       </ul>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div>
-      <div className={styles.mainDiv}>
+    <div className={styles.sourceDiv}>
+      <div className={styles.mainDiv1}>
         <ul>
-          <NavLink to="/">
-            <img
-              src="../Images/logo.png"
-              style={{ width: "170px", height: "23px" }}
-            />
-          </NavLink>
-          <NavLink to="/">Hours</NavLink>
-          <NavLink to="/projects">Projects</NavLink>
-          <li className={styles.dropdwon}>
+          <li>
+            <NavLink to="/">
+              <img
+                src="../Images/logo.png"
+                style={{
+                  width: "170px",
+                  height: "25px",
+                }}
+              />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" style={{ textDecoration: "none" }}>
+              Hours
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" style={{ textDecoration: "none" }}>
+              Projects
+            </NavLink>
+          </li>
+          <li>
             Reports
-            <div className={styles.dropdowncontent}>
-              <Link>1</Link>
-              <Link>2</Link>
-              <Link>3</Link>
-              <Link>4</Link>
-            </div>
+            <ul className={styles.reportDrop}>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
           </li>
-          <li>Manage</li>
           <li>
-            <FiSearch />
+            Manage
+            <ul className={styles.reportDrop}>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </li>
+          <li>
+            <FiSearch style={{ marginTop: "10px" }} />
           </li>
         </ul>
-        <ul>
-          <li>
-            <AiFillPlayCircle />
-          </li>
-          <li>
-            <AiOutlinePlusCircle />
-          </li>
-          <li>
-            <div style={{ border: "1px solid blue" }}>GET PRO</div>
-          </li>
-          <li>
-            <BiDownload />
-          </li>
-          <li>
-            Delete from Data
-            <RiDeleteBin6Line />
-          </li>
-          <li>Profile</li>
-        </ul>
+      </div>
+      <div className={styles.mainDiv2}>
+        <h1>
+          <AiFillPlayCircle />
+        </h1>
+        <h1>
+          <AiOutlinePlusCircle />
+        </h1>
+        <div className={styles.getPro}>GET PRO</div>
+        {/* <BiDownload /> */}
+        <div
+          style={{
+            border: "1px solid blue",
+            borderRadius: "5px",
+            padding: "5px",
+            backgroundColor: "#fff7e5",
+            fontWeight: "bold",
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          Delete from Data
+          <RiDeleteBin6Line style={{ color: "brown", fontSize: "20px" }} />
+        </div>
+        Profile
       </div>
     </div>
   );
